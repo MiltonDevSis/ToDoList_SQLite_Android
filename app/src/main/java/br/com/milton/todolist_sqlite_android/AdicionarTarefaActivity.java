fb224e2c090540af1b09d8e181fb2b312c513ebe
MiltonDevSis <milton.dev.sis@gmail.com>
2020-11-27 15:01:18 -0300

    Criado interface básica com a basic activity

---
+++ app/src/main/java/br/com/milton/todolist_sqlite_android/AdicionarTarefaActivity.java
@@ -0,0 +1,14 @@
+package br.com.milton.todolist_sqlite_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
