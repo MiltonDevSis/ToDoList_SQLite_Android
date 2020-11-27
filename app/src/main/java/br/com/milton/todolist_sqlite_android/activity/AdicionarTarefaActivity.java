fef865a0618b5cffddc297667cd2dc19e8fa0ce9
MiltonDevSis <milton.dev.sis@gmail.com>
2020-11-27 16:45:04 -0300

    Adicionado menu salvar e evento de clique do mesmo

--- app/src/main/java/br/com/milton/todolist_sqlite_android/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/br/com/milton/todolist_sqlite_android/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package br.com.milton.todolist_sqlite_android.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import br.com.milton.todolist_sqlite_android.R;
 
@@ -12,5 +16,22 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId() ){
+
+            case R.id.itemSalvar:
+                Toast.makeText(AdicionarTarefaActivity.this, "Salvo", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
