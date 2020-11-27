e654afb07f0ecd4eda1794e80ef8614b77f42799
MiltonDevSis <milton.dev.sis@gmail.com>
2020-11-27 17:12:56 -0300

    Adicionado banco, configurado com BbHelper.

---
+++ app/src/main/java/br/com/milton/todolist_sqlite_android/helper/DbHelper.java
@@ -0,0 +1,49 @@
+package br.com.milton.todolist_sqlite_android.helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static int VERSION = 1;
+    public static String NOME_DB = "DB_TAREFAS";
+    public static String TABELA_TAREFAS = "tarefas";
+
+    public DbHelper(@Nullable Context context) {
+        super(context, NOME_DB, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABELA_TAREFAS +
+                " (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL); ";
+
+        try {
+            db.execSQL( sql );
+            Log.i("Info DB", "Sucesso ao criar a tabela");
+        }catch (Exception e){
+            Log.i("Info DB", "Erro ao criar a tabela" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        // comando para teste(irá deletar a tabela depois crialá novamente)
+        String sql = "DROP TABLE IF EXISTS " + TABELA_TAREFAS + " ;";
+
+        try {
+            db.execSQL( sql );
+            onCreate( db );
+            Log.i("Info DB", "Sucesso ao deletar tabela");
+        }catch (Exception e){
+            Log.i("Info DB", "Erro ao deletar" + e.getMessage());
+        }
+
+    }
+}
